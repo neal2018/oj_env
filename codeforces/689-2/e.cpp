48fb6c2d8508dd4ad5fb46a5808e81a6eb27af0b
neallai <neal.lion@icloud.com>
2021-11-08 19:21:43 -0500

    689-2-E

--- codeforces/689-2/e.cpp
+++ codeforces/689-2/e.cpp
@@ -7,37 +7,23 @@ int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   ll k, l, r, t, x, y;
   cin >> k >> l >> r >> t >> x >> y;
-  ll day_wo_fill = (k - l) / x;
-  if (day_wo_fill >= t) {
-    cout << "Yes\n";
-    return 0;
-  }
-  ll before_1_fill = k - x * day_wo_fill;
-  ll after_1_fill = before_1_fill + y;
-  if (after_1_fill > r) {
-    cout << "No\n";
-    return 0;
-  }
-  t -= day_wo_fill;
-  {
-    k = after_1_fill;
-    ll day_wo_fill = (k - l) / x;
-    if (day_wo_fill >= t) {
-      cout << "Yes\n";
-      return 0;
-    }
-    ll before_1_fill = k - x * day_wo_fill;
-    ll after_1_fill = before_1_fill + y;
-    if (after_1_fill > r) {
-      cout << "No\n";
-      return 0;
-    }
-    ll diff =after_1_fill - k;
-    if (diff > 0 && r-k <x) {
-    } else if (diff < 0 && k-l) {
+  k -= l, r -= l;
+  if (x > y) {
+    if (k + y <= r) k += y;
+    k -= x;
+    if (k < 0) return cout << "No\n", 0;
+    if (t <= k / (x - y) + 1) {
+      return cout << "Yes\n", 0;
     } else {
-      cout << "Yes\n";
-      return 0;
+      return cout << "No\n", 0;
     }
   }
+  set<ll> st;
+  while (true) {
+    t -= k / x, k %= x;
+    if (t <= 0 || st.count(k)) return cout << "Yes\n", 0;
+    st.insert(k), k += y;
+    if (k > r || k < x) return cout << "No\n", 0;
+  }
+  return 0;
 }
