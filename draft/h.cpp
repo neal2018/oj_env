ebe46f898a39ff467d60fa118153fc8bdc4fb31a
neal <neal.lion@icloud.com>
2022-03-06 01:24:23 -0500

    add linear

--- draft/h.cpp
+++ draft/h.cpp
@@ -1,35 +1,45 @@
 #include <bits/stdc++.h>
-typedef long long ll;
 using namespace std;
+#define ll long long
+constexpr int M = 2e6 + 10, MOD = 1e9 + 7;
+struct C {
+  int f[M] = {1}, rf[M] = {1}, inv[M] = {};
+  constexpr C() {
+    f[1] = 1, rf[1] = 1, inv[1] = 1;
+    for (int i = 2; i < M; i++) {
+      inv[i] = 1ll * (MOD - MOD / i) * inv[MOD % i] % MOD;
+      f[i] = 1ll * f[i - 1] * i % MOD;
+      rf[i] = 1ll * rf[i - 1] * inv[i] % MOD;
+    }
+  }
+};
 
 int main() {
-      cin.tie(nullptr)->sync_with_stdio(false);
-    int n, m, c;
-    cin>>n;
-    vector<int> p(n), a(n), b(n);
-    for(auto&x:p)cin>>x;
-    for(auto&x:a)cin>>x;
-    for(auto&x:b)cin>>x;
-    vector<set<pair<int,int>>> st(4);
-    for(int i=0;i<n;i++){
-        st[a[i]].insert({p[i], i});
-        if (a[i]!=b[i]) st[b[i]].insert({p[i], i});
+  const C c = C();
+  int h, w, n;
+  cin >> h >> n;
+  w = h;
+  int x[n + 1], y[n + 1];
+  for (int i = 0; i < n; i++) {
+    cin >> x[i] >> y[i];
+    x[i]--, y[i]--;
+  }
+  x[n] = h - 1, y[n] = w - 1;
+  int order[n + 1];
+  iota(order, order + n + 1, 0);
+  sort(order, order + n + 1,
+       [&](int a, int b) { return x[a] == x[b] ? y[a] < y[b] : x[a] < x[b]; });
+  int dp[n + 1];  // dp[i]: number of path from [0,0] to [xi, yi]
+  for (int i = 0; i <= n; i++) {
+    int k = order[i];
+    dp[i] = 1ll * c.f[x[k] + y[k]] * c.rf[x[k]] % MOD * c.rf[y[k]] % MOD;
+    // (xi+yi chose xi) = (xi+yi)!/(xi!yi!)
+    for (int j = 0; j < i; j++) {
+      int dx = x[k] - x[order[j]], dy = y[k] - y[order[j]];
+      if (dx < 0 || dy < 0) continue;
+      dp[i] =
+          (MOD + dp[i] - 1ll * dp[j] * c.f[dx + dy] % MOD * c.rf[dx] % MOD * c.rf[dy] % MOD) % MOD;
     }
-    cin>>m;
-    vector<int> res(m, -1);
-    for(int i=0;i<m;i++){
-        cin>>c;
-        if (st[c].size()==0)continue;
-        auto [cc, index] = *st[c].begin();
-        st[c].erase(*st[c].begin());
-        if (st[a[index]].count(pair{p[index], index})){
-            st[a[index]].erase({p[index], index});
-        }
-        if (st[b[index]].count(pair{p[index], index})){
-            st[b[index]].erase(pair{p[index], index});
-        }
-        res[i]=p[index];
-    }
-    for(int i=0;i<m;i++)cout<<res[i]<<" ";
-    cout<<'\n';
+  }
+  cout << dp[n] << endl;
 }
